baa9f29049006da496934a979fd43852a826744b
Harry-258 <haraldtoth1@gmail.com>
2025-11-04 17:53:11 +0100

    Added skeleton for fetching projects and toggling server button

--- src/widgets/test-widget/app.tsx
+++ src/widgets/test-widget/app.tsx
@@ -1,19 +1,41 @@
-import React, {memo, useCallback} from 'react';
+import React, {memo, useCallback, useState, useEffect} from 'react';
 import Button from '@jetbrains/ring-ui-built/components/button/button';
 
-// Register widget in YouTrack. To learn more, see https://www.jetbrains.com/help/youtrack/devportal-apps/apps-host-api.html
 const host = await YTApp.register();
 
 const AppComponent: React.FunctionComponent = () => {
-  const callBackend = useCallback(async () => {
-    const result = await host.fetchApp('backend/debug', {query: {test: '123'}});
-    // eslint-disable-next-line no-console
-    console.log('request result', result);
-  }, []);
+    // TODO: Search functionality for projects?
+
+    const [buttonValue, setButtonValue] = useState(false);
+    const [projects, setProjects] = useState<string[]>([]);
+
+    // TODO: Websockets to make changes realtime?
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const result = await host.fetchYouTrack('projects');
+            setProjects(result.projects);
+        };
+        fetchProjects();
+    }, []);
+
+    // TODO: Websockets to make changes realtime?
+    const toggleBackendFlag = useCallback(async () => {
+        const result = await host.fetchApp('backend/flag', {method: 'POST', body: {value: buttonValue}})
+        console.log(`result is ${result}`);
+        // TODO: check if response is ok
+        if (result) {
+            setButtonValue(!buttonValue);
+        } else {
+            host.alert('Error changing button state!');
+        }
+    }, [buttonValue]);
 
   return (
     <div className="widget">
-      <Button primary onClick={callBackend}>{'Make HTTP Request'}</Button>
+      <Button primary onClick={toggleBackendFlag}>Toggle Backend</Button>
+        { projects.map((project, index) =>
+            <div key={index}>{project}</div>
+        )}
     </div>
   );
 };
